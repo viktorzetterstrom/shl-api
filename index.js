afb567704f210072f1ed8a85d20d6c1d3a1d5d98
Viktor Zetterström <viktor.zetterstrom@footway.com>
2019-09-24 16:04:42 +0200

    add endpoint for standings with redis cache

--- index.js
+++ index.js
@@ -1,16 +1,28 @@
 require('dotenv').config();
 const express = require('express');
+const redis = require('redis');
 const { ShlConnection } = require('./shl-connection');
 const { ShlClient } = require('./shl-client');
+
+const redisClient = redis.createClient(6379);
 const clientId = process.env.CLIENT_ID;
 const clientSecret = process.env.CLIENT_SECRET;
 
 const app = express();
 const shl = new ShlClient(new ShlConnection(clientId, clientSecret));
 
-app.get('/standings', async (req, res) => {
-  const result = await shl.season(2019).statistics.teams.standings();
-  res.send(result);
+app.get('/standings', (req, res) => {
+  const standingsRedisKey = 'shl:standings';
+
+  return redisClient.get(standingsRedisKey, async (err, standings) => {
+    if (standings) {
+      return res.json({ soure: 'cache', data: JSON.parse(standings) });
+    }
+
+    const standingsFromDb = await shl.season(2019).statistics.teams.standings();
+    redisClient.setex(standingsRedisKey, 1800, JSON.stringify(standingsFromDb));
+    return res.json({ source: 'db', data: standingsFromDb });
+  });
 });
 
 app.listen(4000, () => {
