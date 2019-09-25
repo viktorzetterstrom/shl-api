ab48d80f894f5ea63062254d26c4d6b56af35335
Viktor Zetterström <viktor.zetterstrom@footway.com>
2019-09-25 12:40:00 +0200

    add cache lifespan as env variable

--- index.js
+++ index.js
@@ -8,21 +8,23 @@ const redisClient = redis.createClient(6379);
 const clientId = process.env.CLIENT_ID;
 const clientSecret = process.env.CLIENT_SECRET;
 const port = process.env.PORT;
+const cacheLifespan = process.env.CACHE_LIFESPAN;
 
 const app = express();
 const shl = new ShlClient(new ShlConnection(clientId, clientSecret));
 
-app.get('/standings', (req, res) => {
+app.get('/standings', (_, res) => {
   const standingsRedisKey = 'shl:standings';
 
-  return redisClient.get(standingsRedisKey, async (err, standings) => {
-    if (standings) {
-      return res.json({ soure: 'cache', data: JSON.parse(standings) });
-    }
+  return redisClient.get(standingsRedisKey, (err, standings) => {
+    if (err) return res.json({ error: err });
+    if (standings) return res.json({ soure: 'cache', data: JSON.parse(standings) });
 
-    const standingsFromShlApi = await shl.season(2019).statistics.teams.standings();
-    redisClient.setex(standingsRedisKey, 1800, JSON.stringify(standingsFromShlApi));
-    return res.json({ source: 'db', data: standingsFromShlApi });
+    return shl.season(2019).statistics.teams.standings()
+      .then((apiResponse) => {
+        redisClient.setex(standingsRedisKey, cacheLifespan, JSON.stringify(apiResponse));
+        return res.json({ source: 'api', data: apiResponse });
+      });
   });
 });
 
