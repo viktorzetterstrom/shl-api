2d0f4d5c4e9e8dcc5f36d7d35d2cfe2befdb6d47
Viktor Zetterström <viktor.zetterstrom@footway.com>
2019-09-26 14:16:22 +0200

    add name and logo url to response

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const redis = require('redis');
 const { ShlConnection } = require('./shl-connection');
 const { ShlClient } = require('./shl-client');
+const teamInfo = require('./team-info.json');
 
 const redisClient = redis.createClient(6379);
 const clientId = process.env.CLIENT_ID;
@@ -19,11 +20,20 @@ app.get('/api/standings', (_, res) => {
   return redisClient.get(standingsRedisKey, (err, standings) => {
     if (err) return res.json({ error: err });
     if (standings) return res.json({ soure: 'cache', data: JSON.parse(standings) });
-
     return shl.season(2019).statistics.teams.standings()
       .then((apiResponse) => {
-        redisClient.setex(standingsRedisKey, cacheLifespan, JSON.stringify(apiResponse));
-        return res.json({ source: 'api', data: apiResponse });
+        const apiResponseWithTeamInfo = apiResponse.map(team => ({
+          ...team,
+          logo: teamInfo[team.team.id].logo,
+          name: teamInfo[team.team.id].name,
+        }));
+
+        redisClient.setex(
+          standingsRedisKey,
+          cacheLifespan,
+          JSON.stringify(apiResponseWithTeamInfo),
+        );
+        return res.json({ source: 'api', data: apiResponseWithTeamInfo });
       });
   });
 });
