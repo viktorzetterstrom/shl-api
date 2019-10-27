7e7645ff22873b8043399d5e59aaf1420bdc313a
Viktor Zetterström <viktor.zetterstrom@footway.com>
2019-10-27 18:35:41 +0100

    refactor formatting of api-responses to formatter#

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const https = require('https');
 const cors = require('cors');
 const fs = require('fs');
 const redis = require('redis');
+const formatter = require('./shl-api-formatter');
 const { ShlConnection } = require('./shl-connection');
 const { ShlClient } = require('./shl-client');
-const teamInfo = require('./team-info.json');
 
 const redisClient = redis.createClient(6379);
 const clientId = process.env.CLIENT_ID;
@@ -36,23 +36,17 @@ app.get('/standings', cors(corsOptions), (_, res) => {
 
   return redisClient.get(standingsRedisKey, (err, standings) => {
     if (err) return res.json({ error: err });
-    if (standings) {
-      return res.json({ soure: 'cache', data: JSON.parse(standings) });
-    }
+    if (standings) return res.json({ soure: 'cache', data: JSON.parse(standings) });
+
     return shl.season(2019).statistics.teams.standings()
       .then((apiResponse) => {
-        const apiResponseWithTeamInfo = apiResponse.map(team => ({
-          ...team,
-          logo: teamInfo[team.team.id].logo,
-          name: teamInfo[team.team.id].name,
-        }));
-
+        const formatedResponse = formatter.standings(apiResponse);
         redisClient.setex(
           standingsRedisKey,
           cacheLifespan,
-          JSON.stringify(apiResponseWithTeamInfo),
+          JSON.stringify(formatedResponse),
         );
-        return res.json({ source: 'api', data: apiResponseWithTeamInfo });
+        return res.json({ source: 'api', data: formatedResponse });
       });
   });
 });
@@ -67,20 +61,13 @@ app.get('/games', cors(corsOptions), (_, res) => {
     }
     return shl.season(2019).games()
       .then((apiResponse) => {
-        const apiResponseWithTeamInfo = apiResponse.map(game => ({
-          ...game,
-          home_team_logo: teamInfo[game.home_team_code].logo,
-          away_team_logo: teamInfo[game.away_team_code].logo,
-          home_team_name: teamInfo[game.home_team_code].name,
-          away_team_name: teamInfo[game.away_team_code].name,
-        }));
-
+        const formatedResponse = formatter.games(apiResponse);
         redisClient.setex(
           standingsRedisKey,
           cacheLifespan,
-          JSON.stringify(apiResponseWithTeamInfo),
+          JSON.stringify(formatedResponse),
         );
-        return res.json({ source: 'api', data: apiResponseWithTeamInfo });
+        return res.json({ source: 'api', data: formatedResponse });
       });
   });
 });
---
+++ shl-api-formatter.js
@@ -0,0 +1,27 @@
+const teamInfo = require('./team-info.json');
+
+const standings = apiResponse => apiResponse.map(team => ({
+  ...team,
+  logo: teamInfo[team.team.id].logo,
+  name: teamInfo[team.team.id].name,
+}));
+
+const games = apiResponse => apiResponse
+  .filter((game) => {
+    const oneWeekAway = new Date();
+    oneWeekAway.setDate(oneWeekAway.getDate() + 7);
+    const oneWeekAgo = new Date();
+    oneWeekAgo.setDate(oneWeekAgo.getDate() - 7);
+    const gameDate = new Date(game.start_date_time);
+    return oneWeekAgo < gameDate && gameDate < oneWeekAway;
+  })
+  .map(game => ({
+    ...game,
+    home_team_logo: teamInfo[game.home_team_code].logo,
+    away_team_logo: teamInfo[game.away_team_code].logo,
+  }));
+
+module.exports = {
+  standings,
+  games,
+};
