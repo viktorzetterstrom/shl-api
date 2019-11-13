7511a96932fb24cab874b19a4107990472220b36
Viktor Zetterström <viktor.zetterstrom@footway.com>
2019-11-13 19:06:06 +0100

    add winstreaks for home and away games

--- shl-api-formatter.js
+++ shl-api-formatter.js
@@ -53,31 +53,53 @@ const players = apiResponse => apiResponse
 
 const winstreaks = (apiResponse) => {
   const playedGames = apiResponse.filter(game => new Date() > new Date(game.start_date_time));
-  const teamWinstreaks = {};
-  const hasLost = [];
-  playedGames.forEach((game) => {
-    const winner = game.home_team_result > game.away_team_result
-      ? game.home_team_code
-      : game.away_team_code;
-    const loser = game.home_team_result < game.away_team_result
-      ? game.home_team_code
-      : game.away_team_code;
+  const teamWinstreaks = { ...teamInfo };
+  Object.keys(teamInfo).forEach((teamCode) => {
+    teamWinstreaks[teamCode].streaks = {
+      all: 0,
+      home: 0,
+      away: 0,
+    };
+  });
+  console.dir(teamWinstreaks);
 
-    hasLost.push(loser);
-    if (!hasLost.includes(winner)) {
-      teamWinstreaks[winner] = teamWinstreaks[winner] !== undefined
-        ? {
-          ...teamWinstreaks[winner],
-          winstreak: teamWinstreaks[winner].winstreak + 1,
-        }
-        : {
-          name: teamInfo[winner].name,
-          logo: teamInfo[winner].logo,
-          winstreak: 1,
-        };
+  const hasLostHome = {};
+  const hasLostAway = {};
+
+  playedGames.forEach((game) => {
+    debugger;
+    const homeCode = game.home_team_code;
+    const awayCode = game.away_team_code;
+    const homeResult = game.home_team_result;
+    const awayResult = game.away_team_result;
+
+    if (homeResult > awayResult) {
+      hasLostAway[awayCode] = true;
+
+      teamWinstreaks[homeCode].streaks = {
+        ...teamWinstreaks[homeCode].streaks,
+        all: !hasLostHome[homeCode] && !hasLostAway[homeCode]
+          ? teamWinstreaks[homeCode].streaks.all += 1
+          : teamWinstreaks[homeCode].streaks.all,
+        home: !hasLostHome[homeCode]
+          ? teamWinstreaks[homeCode].streaks.home += 1
+          : teamWinstreaks[homeCode].streaks.home,
+      };
+    } else {
+      hasLostHome[homeCode] = true;
+
+      teamWinstreaks[awayCode].streaks = {
+        ...teamWinstreaks[awayCode].streaks,
+        all: !hasLostHome[awayCode] && !hasLostAway[awayCode]
+          ? teamWinstreaks[awayCode].streaks.all += 1
+          : teamWinstreaks[awayCode].streaks.all,
+        away: !hasLostAway[awayCode]
+          ? teamWinstreaks[awayCode].streaks.away += 1
+          : teamWinstreaks[awayCode].streaks.away,
+      };
     }
   });
-  return Object.values(teamWinstreaks).sort((a, b) => b.winstreak - a.winstreak);
+  return Object.values(teamWinstreaks).sort((a, b) => b.streaks.all - a.streaks.all);
 };
 
 module.exports = {
